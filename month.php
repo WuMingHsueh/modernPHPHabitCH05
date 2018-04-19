fb6926f85e15f95da23c98bc16451971b72e2386
Wu Ming-Hsueh <ri3567@gmail.com>
2018-04-19 17:49:34 +0800

    fix month
    加入處理-3 -6 -12

--- month.php
+++ month.php
@@ -1,12 +1,25 @@
 <?php
-$monthDiff = -3;
+$monthDiff = 12;
 
-$date = DateTime::createFromFormat("Y-m-d", "2018-03-27", new DateTimeZone("Asia/Taipei"));
+$date = DateTime::createFromFormat("Y-m-d", "2018-03-31", new DateTimeZone("Asia/Taipei"));
 $date2 = clone $date;
 $date2->add(DateInterval::createFromDateString($monthDiff." months"));
 
-if (((int) ($date->format('m')) + $monthDiff) != $date2->format('m')) {
-    echo $date2->format('Y') . '-' . ($date->format('m') + $monthDiff) . '-' . date('t', strtotime($date2->format('Y').'-'. ($date->format('m') + $monthDiff).'-'.'01'));
+$theTrueMonth = $date->format('n') + $monthDiff;
+echo $theTrueMonth. "<hr>";
+if ($theTrueMonth > 12) {
+    $theTrueMonth %= 12;
+}
+while ($theTrueMonth < 0) {
+    $theTrueMonth += 12;
+}
+if ($theTrueMonth == 0) {
+    $theTrueMonth = 12;
+}
+echo $theTrueMonth. "<hr>";
+
+if (($theTrueMonth) != $date2->format('n')) {
+    echo $date2->format('Y') . '-' . $theTrueMonth . '-' . date('t', strtotime($date2->format('Y').'-'. $theTrueMonth .'-'.'01'));
 } else {
     echo $date2->format('Y-m-d');
 }
